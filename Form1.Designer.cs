ce3e643b7fca81a5eca4f9df31b1a31f42c37235
PabloPseudoEkspert <czarnybbc@gmail.com>
2023-01-26 13:39:11 +0100

    Work

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.open = new System.Windows.Forms.ToolStripMenuItem();
+            this.save = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 31);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(775, 407);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menu
+            // 
+            this.menu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.open,
+            this.save,
+            this.fontMenu,
+            this.fontColor,
+            this.Exit});
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(60, 26);
+            this.menu.Text = "Menu";
+            // 
+            // open
+            // 
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(224, 26);
+            this.open.Text = "Open";
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // save
+            // 
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(224, 26);
+            this.save.Text = "Save";
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // fontColor
+            // 
+            this.fontColor.Name = "fontColor";
+            this.fontColor.Size = new System.Drawing.Size(224, 26);
+            this.fontColor.Text = "Font Color";
+            this.fontColor.Click += new System.EventHandler(this.fontColor_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(224, 26);
+            this.Exit.Text = "Exit";
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // fontMenu
+            // 
+            this.fontMenu.Name = "fontMenu";
+            this.fontMenu.Size = new System.Drawing.Size(224, 26);
+            this.fontMenu.Text = "Fonts";
+            this.fontMenu.Click += new System.EventHandler(this.fontMenu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menu;
+        private System.Windows.Forms.ToolStripMenuItem open;
+        private System.Windows.Forms.ToolStripMenuItem save;
+        private System.Windows.Forms.ToolStripMenuItem fontColor;
+        private System.Windows.Forms.ToolStripMenuItem Exit;
+        private System.Windows.Forms.ToolStripMenuItem fontMenu;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,63 @@ namespace Task_4
 {
     public partial class Form1 : Form
     {
+        private object fontDialog;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text File (.txt)|*.txt";
+            ofd.Title= "Open file";
+
+            if (ofd.ShowDialog() == DialogResult.OK) 
+            { 
+                System.IO.StreamReader sr = new System.IO.StreamReader(ofd.FileName);
+                richTextBox1.Text= sr.ReadToEnd();
+                sr.Close();
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text File (.txt)|*.txt";
+            sfd.Title = "Save File";
+
+            if (sfd.ShowDialog() == DialogResult.OK) 
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(sfd.FileName);
+                sw.WriteLine(richTextBox1.Text);
+                sw.Close();
+            }
+        }
+
+        private void fontMenu_Click(object sender, EventArgs e)
+        {
+            DialogResult fd = fontDialog1.ShowDialog();
+            if (fd == DialogResult.OK)
+            {
+                richTextBox1.Font= fontDialog1.Font;
+            }
+        }
+
+        private void fontColor_Click(object sender, EventArgs e)
+        {
+            DialogResult cd = colorDialog1.ShowDialog();
+
+            if (cd == DialogResult.OK)
+            {
+                richTextBox1.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
